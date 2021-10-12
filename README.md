7d2f137c4071814048aec462bcf57898b29a41df
valkyria <vaaalkens@gmail.com>
2021-10-13 04:00:34 +0800

    Update files

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 --------
 
-The result of what happens when you don't trust third-party modules, vStore is an object-oriented datastore wrapper designed to interface well with Roblox's datastores. This is loosely based on loleris' [ProfileService](https://github.com/MadStudioRoblox/ProfileService) module.
+The result of what happens when I don't trust third-party modules and choose to write one myself, vStore is an object-oriented datastore wrapper designed to interface well with Roblox's datastores. This is loosely based on loleris' [ProfileService](https://github.com/MadStudioRoblox/ProfileService) module.
 
 
 
@@ -12,23 +12,19 @@ The result of what happens when you don't trust third-party modules, vStore is a
 ### **Object-Oriented**
 > Spawn new sessions and mutate data using vStore for each player/object like as if you are changing the properties of a part. 
 > 
-> Manage and view data in a way that's clean, efficient and readable.
+> Manage and view data in a way that's clean, efficient and readable, using the syntax you're most familiar with.
 
 ### **Feature-Ready**
-> Equipped with features such as caching, datastore 2.0, and more importantly session locking, vStore ensures your games remain most up-to-date, performant, and reliable.
+> Equipped with features such as:
+> - caching
+> - datastore 2.0
+> - automatic retrying
+> - session locking
+> 
+> vStore ensures your games remain most performant, up-to-date, and reliable.
 
 ### **Easy to learn**
-> vStore is heavily encapsulated and does the heavy lifting behind the scenes for you, so you only need to worry about the important things - getting datastore work done quickly and cleanly.
-> 
-> The syntax is also designed to resemble and use syntax you're most probably familiar with, so you can get to work easily.
-
-### **Versatile**
-> Why use vStore for just player data? Use it on just about anything, be it parts, objects, etc.
-
-### **Customizable**
-> vStore is designed for versatility - customize it as you wish to suit your tastes. Want your own getter/setter functions? Write them and plug it into the module! 
-> 
-> vStore is also well documented for the times when you need that extra functionality, so you can add on to the module with relative ease.
+> vStore is well encapsulated and does the heavy lifting behind the scenes for you, so you only need to worry about the important things - getting datastore work done quickly and cleanly.
 
 ### **Made and used by me!**
 > Because I use this module heavily myself, whatever patches I release will also be published for all to use. 
--- ServerLibrary/Session.lua
+++ ServerLibrary/Session.lua
@@ -13,88 +13,90 @@ local cache = nil
 Session = {}
 Session.__index = Session
 
-Session.AllSessions = {}
-
 -- Internals
 local function InternalGet(session)
-	local success, results, info = pcall(function()
-		return session.Datastore.UpdateAsync(session.Datastore, session.Id, function(old)
-			if Session.Getter ~= nil then Session.Getter(old)
-			else 
-				return old
-			end
-		end)
-	end)
+	local cap = limit
+	local success, results, info
 
-	if not success then warn(string.format("Datastore load ERROR for %s - %s", session.Id, results))
-	else
-		if info == nil then warn(string.format("Datastore load WARNING for %s - no data exists!", session.Id)) 
-		else 
-			print(string.format("Datastore load SUCCESS for %s.", session.Id)) 
-			return results, info
-		end
+	print(string.format("Attempt to load for %s...", session.Id))
+
+	while not success and cap < limit do 
+		success, results, info = pcall(function()
+			return session.Datastore.UpdateAsync(session.Datastore, session.Id, function(old)
+				return old
+			end)
+		end)
+		
+		if success then break end
+	end
+	
+	if cap >= limit then
+		warn(string.format("Datastore load ERROR for %s - %s", session.Id, results)) 
+		warn(string.format("A new default data is generated for %s. This will not save!", session.Id)) 
+		
+		session.Enabled = false
+		
+		return defaults, info
+	end
+	
+	if results == nil then
+		warn(string.format("Datastore load WARNING for %s - no data exists.", session.Id))
+		warn(string.format("A default data set will be provided for %s.", session.Id))
+		return defaults, info
+	else 
+		print(string.format("Datastore load SUCCESS for %s.", session.Id)) 
+		return results, info
 	end
 end
 local function InternalSet(session, dict)
+	print(string.format("Attempt to save for %s...", session.Id))
+
 	local success, results, info = pcall(function()
-		return session.Datastore.UpdateAsync(session.Datastore, session.Id, function(old)
-			if Session.Setter ~= nil then Session.Setter(old)
-			else 
-				local new = dict
-				return new
-			end
-		end)
+		if session.Enabled == true then
+			return session.Datastore.UpdateAsync(session.Datastore, session.Id, function(old)
+				return dict
+			end)
+		else warn(string.format("Datastore save WARNING for %s - save is disabled.", session.Id)) end
 	end)
 
 	if not success then warn(string.format("Datastore save ERROR for %s - %s", session.Id, results))
 	else print(string.format("Datastore save SUCCESS for %s.", session.Id)) end
 end
 
--- Property functions
+-- Properties
 do
-	Session.Getter = nil
-	Session.Setter = nil
-
 	Session.GetDefaults = function()
 		return defaults
 	end
 	Session.SetDefaults = function(dict)
 		defaults = dict
-		return defaults
 	end
 	Session.GetCache = function()
 		return cache
 	end
 	Session.SetCache = function(c)
 		cache = require(c)
-		return cache
 	end
-	Session.GetSession = function(id)
-		return Session.AllSessions[tostring(id)]
+	Session.GetLimit = function()
+		return limit
+	end
+	Session.SetLimit = function(l)
+		limit = l
 	end
 end
 
 
---[[ <<CONSTRUCTOR>> Session.new(datastoreName, id)
-	PARAMETERS: 
-	- <string> datastoreName : The name of the target datastore.
-	- <number> id : The number for unique identification.
-
-	RETURNS:
-	- <table> Session : A table-as-object containing session properties and methods.
-
-	* This constructor should be the default way of spawning a new Session object.
-	Instantiates a new Session object. ]] 
 function Session.new(datastoreName: string, id: number?)
 	if cache == nil then error("Cache is nil!") end
-	
+	if defaults == nil then error("Defaults is nil!") end
+	if limit == nil or limit <= 0 then error("Limit is nil or is/under the value of 0!") end
+
 	local newSession = {}
 
 	newSession.Id = tostring(id)
 	newSession.Datastore = Datastore:GetDataStore(datastoreName, id, Instance.new("DataStoreOptions"):SetExperimentalFeatures({["v2"] = true}))
 	newSession.Lock = game.JobId .. "_" .. os.time()
-
-	Session.AllSessions[newSession.Id] = newSession
+	newSession.Enabled = true
 
 	return setmetatable(newSession, Session)
 end
@@ -103,32 +105,9 @@ end
 -- GET methods
 do
 	function Session:GetFromStoreAsync()
-		cache[self.Id] = InternalGet(self)
-		return cache[self.Id]
-	end
+		local res = InternalGet(self)
 
-	function Session:GetFromStoreAsyncMore()
-		cache[self.Id] = InternalGet(self)
-		print(cache[self.Id])
-		return cache[self.Id]
-	end
-
-	function Session:GetFromStoreAsyncVerbose(format: string, locale: string)
-		local data, info = InternalGet(self)
-
-		local f = format or "LLL"
-		local l = locale or "en-us"
-
-		cache[self.Id] = data
-
-		print(cache[self.Id])
-		print(string.format("Datastore Name - %s", self.Datastore.Name))
-		print(string.format("Datastore Version - %s", info.Version))
-		print(string.format("Datastore Creation Time - %s", tostring(DateTime.fromUnixTimestampMillis(info.CreatedTime):FormatUniversalTime(f, l))))
-		print(string.format("Datastore Updation Time - %s", tostring(DateTime.fromUnixTimestampMillis(info.UpdatedTime):FormatUniversalTime(f, l))))
-		--print(info:GetUserIds())
-		--print(info:GetMetadata())
-
+		cache[self.Id] = res
 		return cache[self.Id]
 	end
 end
@@ -139,9 +118,9 @@ do
 		InternalSet(self, cache[self.Id])
 	end
 
-	function Session:InitializeAsync(dict)
-		InternalSet(self, dict)
-		return self:GetFromStoreAsync()
+	function Session:KillAsync()
+		cache[self.Lock] = 0
+		InternalSet(self, cache[self.Id])
 	end
 end
 
@@ -158,9 +137,7 @@ end
 -- ADMIN methods
 do
 	function Session:WipeStoreAsync()
-		local success, results = pcall(function()
-			self.Datastore:SetAsync(self.Id, 0)
-		end)
+		local success, results = pcall(self.Datastore.RemoveAsync(self.Datastore, self.Id))
 
 		if not success then warn(string.format("Datastore wipe error for %s - %s", self.Id, results))
 		else print(string.format("Datastore wipe success for %s.", self.Id)) end
@@ -169,10 +146,6 @@ do
 	function Session:ClearCache()
 		cache[self.Id] = nil
 		warn(string.format("Cache for %s has been cleared", self.Id))
-	end
-
-	function Session:Kill()
-		Session.AllSessions[self.Id] = nil
 	end
 end
 
